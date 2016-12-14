8a3a289ae4040e6575f6211f521696abe98bbae5
kimajakobsen <kimajakobsen@gmail.com>
2016-12-14 21:43:24 +0100

    now provenance identifeirs are generated for "vertical" partitioned provenance data

--- src/main/java/dk/aau/cs/extbi/casper/SSBProcessor.java
+++ src/main/java/dk/aau/cs/extbi/casper/SSBProcessor.java
@@ -19,10 +19,9 @@ import org.apache.jena.update.UpdateFactory;
 import org.apache.jena.update.UpdateRequest;
 
 public class SSBProcessor {
-	private HashSet<String> custkeys = new HashSet<String>();
-	private HashSet<String> datekeys = new HashSet<String>();
-	private HashSet<String> partkeys = new HashSet<String>();
-	private HashSet<String> suppkeys = new HashSet<String>();
+	private HashSet<String> lokeys = new HashSet<String>();
+	private HashSet<String> dimensions = new HashSet<String>();
+	private HashSet<String> crap = new HashSet<String>();
 	private ArrayList<String> URLs = new ArrayList<String>();
 	private String path;
 	private Dataset dataset;
@@ -34,33 +33,42 @@ public class SSBProcessor {
 	public SSBProcessor (String path) {
 		this.path = path;
 		dataset = TDBFactory.createDataset(this.path);
-		deleteFullMaterialized(dataset);
 	}
 	
 	public void readData(int percentage) {
 		this.percentage = percentage;
 		dataset.begin(ReadWrite.READ) ;
 		
-		
 		this.numberOfFacts = convertToNumberOfFacts(percentage);
 		System.out.println(numberOfFacts);
-		
-		
-		//lineorderDefault();
 		lineorderMod();
+		System.out.println("lineorder keys: "+lokeys.size());
 	
-		//TODO it is a bit weird that it says customer and not custkey (maybe it amounts to the same thing)
-		executeQuery(getCustomers(),"customer",custkeys);
+		for (String string : lokeys) {
+			executeQuery(getAttributes(string),dimensions);
+		}
+		System.out.println("dimensions "+ dimensions.size());
 		
-		executeQuery(getPart(),"partkey",partkeys);
-	
-		executeQuery(getSupplier(),"suppkey",suppkeys);
-		
-		executeQuery(getDate(),"date",datekeys);
+		for (String string : dimensions) {
+			executeQuery(getAttributes(string),crap);
+		}
+		System.out.println("URLs: " + URLs.size());
 		
 		dataset.commit();
 		dataset.end();
 		dataRead = true;
+	}
+
+	private String getAttributes(String string) {
+		String query = ""+
+		" select ?g ?o " +
+				" WHERE { " +
+				"  GRAPH ?g { " +
+				"<"+string+"> " + "   ?p ?o  " +
+				"  } " +
+				" } ";
+
+		return query;
 	}
 
 	private void lineorderMod() {
@@ -69,65 +77,41 @@ public class SSBProcessor {
 		
 		
 		int counter = 0;
-		int lo = 0;
 		int numberOfMatches = this.percentage/10;
 		while (lineorder.hasNext())
 		{
 			QuerySolution bindings = lineorder.nextSolution();
 			if (counter%10 < numberOfMatches) {
 				//System.out.println("counter: "+counter+ " mod 10 = "+counter%10+" is less or equals than "+numberOfMatches);
-				
-				suppkeys.add(bindings.get("suppkey").toString());
-				custkeys.add(bindings.get("customer").toString());
-				datekeys.add(bindings.get("commitdate").toString());
-				datekeys.add(bindings.get("orderdate").toString());
-				partkeys.add(bindings.get("partkey").toString());
-				URLs.add(bindings.get("loGraph").toString());
-				lo++;
-				//System.out.println(lo);
+				lokeys.add(bindings.get("li").toString());
 			}
 			counter++;
 		}
-		System.out.println("Added "+ lo +" graphs from lineitem");
-		
 	}
 
-	@SuppressWarnings("unused")
-	private void lineorderDefault() {
-		qexec = QueryExecutionFactory.create(getLineorder(numberOfFacts), dataset);
-		ResultSet lineorder = qexec.execSelect() ;
-		
-		int lo = 0;
-		while (lineorder.hasNext())
-		{
-			QuerySolution bindings = lineorder.nextSolution();
-			suppkeys.add(bindings.get("suppkey").toString());
-			custkeys.add(bindings.get("customer").toString());
-			datekeys.add(bindings.get("commitdate").toString());
-			datekeys.add(bindings.get("orderdate").toString());
-			partkeys.add(bindings.get("partkey").toString());
-			URLs.add(bindings.get("loGraph").toString());
-			lo++;
-		}
-		System.out.println("Added "+ lo +" graphs from lineitem");
-	}
 
-	private void executeQuery(String query, String primarykey, HashSet<String> keys) {
+	private void executeQuery(String query, HashSet<String> keys) {
 		qexec = QueryExecutionFactory.create(query, dataset) ;
 		ResultSet resultSet = qexec.execSelect() ;
 		
-		int d = 0;
 		while (resultSet.hasNext())
 		{
 			QuerySolution bindings = resultSet.nextSolution();
-			if (keys.contains(bindings.get(primarykey).toString())) {
-				URLs.add(bindings.get("graph").toString());
-				d++;
+			URLs.add(bindings.get("g").toString());
+			String object = bindings.get("o").toString();
+			if (isURL(object)) {
+				keys.add(bindings.get("o").toString());
 			}
 		}
-		System.out.println("Added "+ d +" graphs ("+primarykey+")");
 	}
 	
+	private boolean isURL(String object) {
+		if (object.contains(".com")) {
+			return true;
+		}
+		return false;
+	}
+
 	private int convertToNumberOfFacts(int percentage) {
 		qexec = QueryExecutionFactory.create(getNumberOfFacts(), dataset) ;
 		ResultSet facts = qexec.execSelect() ;
@@ -139,56 +123,7 @@ public class SSBProcessor {
 		}
 		return (numberOfFacts*percentage)/100 ;
 	}
-
-	private void deleteFullMaterialized(Dataset dataset) {
-    	dataset.begin(ReadWrite.WRITE) ;
-    	UpdateRequest request = UpdateFactory.create() ;
-    	request.add("DROP GRAPH <http://example.com/fullMaterilized>") ;
-
-    	// And perform the operations.
-    	UpdateAction.execute(request, dataset) ;
-    	dataset.commit();
-	}
 	
-	private String getSupplier() {
-    	String query =""+
-    			"select ?suppkey ?graph {" +
-    			"    GRAPH ?graph {" +
-    			"        ?suppkey <http://example.com/supkey> ?supp" +
-    			"    }" +
-    			"}" ;
-    	return query;
-	}
-
-	private String getDate() {
-		String query =""+
-    			"select ?date ?graph {" +
-    			"    GRAPH ?graph {" +
-    			"        ?date <http://example.com/date> ?supp" +
-    			"    }" +
-    			"}" ;
-    	return query;
-	}
-
-	private String getPart() {
-		String query =""+
-    			"select ?partkey ?graph {" +
-    			"    GRAPH ?graph {" +
-    			"        ?partkey <http://example.com/partkey> ?supp" +
-    			"    }" +
-    			"}" ;
-    	return query;
-	}
-
-	private String getCustomers() {
-		String query =""+
-    			"select ?customer ?graph {" +
-    			"    GRAPH ?graph {" +
-    			"        ?customer <http://example.com/custkey> ?supp" +
-    			"    }" +
-    			"}" ;
-    	return query;
-	}
 	
 	private String getNumberOfFacts() {
 		String query = ""+
@@ -200,48 +135,17 @@ public class SSBProcessor {
     	return query;
 	}
 
-	private String getLineorder(int numberOfFacts) {
-    	String query = ""+
-    		    "select distinct ?a ?loGraph ?suppkey ?partkey ?customer ?orderdate ?commitdate { " +
-    		    "    Graph ?metadata {" +
-    		    "        ?a  <http://example.com/custkey> ?customer ;" +
-    		    "            <http://example.com/partkey> ?partkey ;" +
-    		    "            <http://example.com/suppkey> ?suppkey ;" +
-    		    "            <http://example.com/orderdate> ?orderdate ;" +
-    		    "            <http://example.com/commitdate> ?commitdate ." +
-    		    "        {" +
-    		    "            Select distinct ?a ?loGraph      " +
-    		    "            { " +
-    		    "                GRAPH ?loGraph {" +
-    		    "                    ?a <http://example.com/revenue> ?c" +
-    		    "                }" +
-    		    "            }" +
-    		    "            limit "+numberOfFacts+
-    		    "        } " +
-    		    "    }" +
-    		    "}" ;
-    	return query;
-    }
+	
 	
 	private String getAllLineorders() {
     	String query = ""+
-    		    "select distinct ?a ?loGraph ?suppkey ?partkey ?customer ?orderdate ?commitdate { " +
-    		    "    Graph ?metadata {" +
-    		    "        ?a  <http://example.com/custkey> ?customer ;" +
-    		    "            <http://example.com/partkey> ?partkey ;" +
-    		    "            <http://example.com/suppkey> ?suppkey ;" +
-    		    "            <http://example.com/orderdate> ?orderdate ;" +
-    		    "            <http://example.com/commitdate> ?commitdate ." +
-    		    "        {" +
-    		    "            Select distinct ?a ?loGraph      " +
-    		    "            { " +
-    		    "                GRAPH ?loGraph {" +
-    		    "                    ?a <http://example.com/revenue> ?c" +
-    		    "                }" +
-    		    "            }" +
-    		    "        } " +
-    		    "    }" +
-    		    "}" ;
+    			"SELECT ?li  " +
+    			"WHERE { " +
+    			"	GRAPH ?g1 { " +
+    			"		?li a <http://purl.org/linked-data/cube#Observation> " +
+    			"	} " +
+    			"} " +
+    			"GROUP BY ?li " ;
     	return query;
     }
 	
